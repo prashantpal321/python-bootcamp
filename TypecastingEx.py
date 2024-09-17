b109457817c220820a052d2ba579cccb5c6b0cef
prashantpal321 <palprashant279@gmail.com>
2024-09-17 19:15:05 +0530

    Add files via upload

---
+++ TypecastingEx.py
@@ -0,0 +1,10 @@
+# A typecasting is a method which we can change the data type to another data type with
+#  the help of typecasting.
+
+# Program
+
+a = "1"
+b = "2"
+c = (int(a)+int(b))
+print(c)
+
